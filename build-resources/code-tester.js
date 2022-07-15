e20269ba738f061eacb2ed2306b884f384e77fc6
Wehi <notstrangequark@gmail.com>
2022-07-15 02:08:14 +0200

    working navigation in code-tester

--- build-resources/code-tester.js
+++ build-resources/code-tester.js
@@ -1,31 +1,59 @@
 const fs = require("fs");
 const path = require("path");
 
-let test = [];
-let dirname = path.dirname(
-  `M:\\Creative\\Coding\\Fullstack\\nextjs\\personal-website\\pages\\pages`
-);
+// let test = [];
+// let dirname = path.dirname(
+//   `M:\\Creative\\Coding\\Fullstack\\nextjs\\personal-website\\pages\\pages`
+// );
 
-// fs.readdir(dirname, (err, files) => {
-//   files.forEach((file) => {
-//     if (file === "_app.js" || file === "index.js") {
-//       return;
-//     } else {
-//       file.replace(/\.js$/, "");
-//       test.push(file);
-//     }
-//   });
+// // fs.readdir(dirname, (err, files) => {
+// //   files.forEach((file) => {
+// //     if (file === "_app.js" || file === "index.js") {
+// //       return;
+// //     } else {
+// //       file.replace(/\.js$/, "");
+// //       test.push(file);
+// //     }
+// //   });
+// // });
+
+// let files = fs.readdirSync(dirname);
+
+// files.forEach((file) => {
+//   file.replace(/\.js$/, "");
+//   if (file === "_app.js" || file === "index.js") {
+//     return;
+//   } else {
+//     test.push(file);
+//   }
 // });
 
-let files = fs.readdirSync(dirname);
+// console.log(test);
 
-files.forEach((file) => {
-  file.replace(/\.js$/, "");
-  if (file === "_app.js" || file === "index.js") {
-    return;
-  } else {
-    test.push(file);
-  }
-});
+  var pagesArray = [];
 
-console.log(test);
+  let dirname = path.dirname(
+    `M:\\Creative\\Coding\\Fullstack\\nextjs\\personal-website\\pages\\pages`
+  );
+
+  let files = fs.readdirSync(dirname);
+
+  files.forEach((file) => {
+    file.replace(/\.js$/, "");
+    if (file === "_app.js" || file === "index.js") {
+      return;
+    } else {
+      pagesArray.push(file);
+    }
+  });
+
+  console.log(pagesArray);
+
+
+let navigation = [
+  { name: "Home", href: "/", current: true },
+  { name: pagesArray[0], href: "/projects", current: false },
+  { name: "About Me", href: "#", current: false },
+];
+
+console.log(navigation); 
